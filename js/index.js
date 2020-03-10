549fe9bf29252cdbc616a5b77ec79db35937bc4b
sid2015980 <sidneybroadnax@gmail.com>
2020-03-10 16:37:04 -0700

    cloned

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-// Your code goes here
+function clickEventHandler(event) {
+    console.log(event.type + '!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+    console.log(event.target)
+
+    document.body.innerHTML = "<div>HAHAHA</div>"
+  }
+
+  document.querySelector('a').addEventListener('click', clickEventHandler)
+  document.querySelector('nav').addEventListener('click', clickEventHandler)
+  document.querySelector('header').addEventListener('click', clickEventHandler)
+  document.body.addEventListener('click', clickEventHandler)
+  document.addEventListener('click', clickEventHandler)
+  window.addEventListener('click', clickEventHandler)
