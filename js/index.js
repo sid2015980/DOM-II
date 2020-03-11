d6d6ecaa7cb70ae819881c3bdcbbbd1da790ec8e
sid2015980 <sidneybroadnax@gmail.com>
2020-03-10 23:01:55 -0700

    finished mouseover

--- js/index.js
+++ js/index.js
@@ -1,13 +1,26 @@
-function clickEventHandler(event) {
-    console.log(event.type + '!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-    console.log(event.target)
+// function clickEventHandler(event) {
+//     console.log(event.type + '!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+//     console.log(event.target)
 
-    document.body.innerHTML = "<div>HAHAHA</div>"
-  }
+//     // document.body.innerHTML = "<div>HAHAHA</div>" destroys all content
+//   }
 
-  document.querySelector('a').addEventListener('click', clickEventHandler)
-  document.querySelector('nav').addEventListener('click', clickEventHandler)
-  document.querySelector('header').addEventListener('click', clickEventHandler)
-  document.body.addEventListener('click', clickEventHandler)
-  document.addEventListener('click', clickEventHandler)
-  window.addEventListener('click', clickEventHandler)
+//   document.querySelector('a').addEventListener('click', clickEventHandler)
+//   document.querySelector('nav').addEventListener('click', clickEventHandler)
+//   document.querySelector('header').addEventListener('click', clickEventHandler)
+//   document.body.addEventListener('click', clickEventHandler)
+//   document.addEventListener('click', clickEventHandler)
+//   window.addEventListener('click', clickEventHandler)
+
+const mouseOver = document.querySelector('.nav');
+
+mouseOver.addEventListener("mouseover", function (event) {
+    event.target.style.color = "gold"
+
+    setTimeout(function () {
+        event.target.style.color = "";
+    }, 500);
+}, false);
+
+
+
